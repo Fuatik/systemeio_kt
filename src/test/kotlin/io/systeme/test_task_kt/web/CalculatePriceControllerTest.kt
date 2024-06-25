83e0a68a3b404f9c3e3bd17679ffa3bcac7264f3
Fuat_Safiulin <fuatsafiulin@gmail.com>
2024-06-25 22:25:54 +0100

    H2 database for tests

--- src/test/kotlin/io/systeme/test_task_kt/web/CalculatePriceControllerTest.kt
+++ src/test/kotlin/io/systeme/test_task_kt/web/CalculatePriceControllerTest.kt
@@ -12,6 +12,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.MediaType
 import org.springframework.http.ProblemDetail
+import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
@@ -20,6 +21,7 @@ import org.springframework.transaction.annotation.Transactional
 
 @SpringBootTest
 @Transactional
+@ActiveProfiles("test")
 @AutoConfigureMockMvc(addFilters = false)
 internal class CalculatePriceControllerTest {
 
--- src/test/kotlin/io/systeme/test_task_kt/web/PurchaseControllerTest.kt
+++ src/test/kotlin/io/systeme/test_task_kt/web/PurchaseControllerTest.kt
@@ -11,6 +11,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.MediaType
 import org.springframework.http.ProblemDetail
+import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
@@ -19,6 +20,7 @@ import org.springframework.transaction.annotation.Transactional
 
 @SpringBootTest
 @Transactional
+@ActiveProfiles("test")
 @AutoConfigureMockMvc(addFilters = false)
 internal class PurchaseControllerTest {
 
