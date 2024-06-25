64b933677a3e09dfb4baf960d1d792790e28d081
Fuat_Safiulin <fuatsafiulin@gmail.com>
2024-06-25 16:52:14 +0100

    populate data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,18 @@
+-- Insert products
+INSERT INTO product (id, name, price)
+VALUES (1, 'iPhone', 100),
+       (2, 'Headphones', 20),
+       (3, 'Case', 10);
+
+-- Insert coupons
+INSERT INTO coupon (id, code, discount, is_percentage)
+VALUES (4, 'D15', 15, TRUE),
+       (5, 'P10', 10, TRUE),
+       (6, 'F5', 5, FALSE);
+
+-- Insert taxes
+INSERT INTO tax_rate (id, region, rate)
+VALUES (7, 'DE', 0.19),
+       (8, 'IT', 0.22),
+       (9, 'FR', 0.20),
+       (10, 'GR', 0.24);
