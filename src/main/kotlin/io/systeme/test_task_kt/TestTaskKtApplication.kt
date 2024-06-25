4552bd24bc47c04c0f67f4b001dbae3f22fe25f9
Fuat_Safiulin <fuatsafiulin@gmail.com>
2024-06-25 16:25:25 +0100

    init

---
+++ src/main/kotlin/io/systeme/test_task_kt/TestTaskKtApplication.kt
@@ -0,0 +1,11 @@
+package io.systeme.test_task_kt
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TestTaskKtApplication
+
+fun main(args: Array<String>) {
+    runApplication<TestTaskKtApplication>(*args)
+}
